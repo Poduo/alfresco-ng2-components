e106c40df981f92f69ac0b6031f94e672d9752fa
Mario Romano <dnamario@hotmail.com>
2016-12-12 11:15:55 +0000

    webpack

--- demo-shell-ng2/src/app/components/index.ts
+++ demo-shell-ng2/src/app/components/index.ts
@@ -19,7 +19,7 @@ export { DataTableDemoComponent } from './datatable/datatable-demo.component';
 export { SearchComponent } from './search/search.component';
 export { SearchBarComponent } from './search/search-bar.component';
 export { LoginDemoComponent } from './login/login-demo.component';
-export { ActivitiDemoComponent } from './activiti/activiti-demo.component';
+// export { ActivitiDemoComponent } from './activiti/activiti-demo.component';
 export { FormViewer } from './activiti/form-viewer.component';
 export { WebscriptComponent } from './webscript/webscript.component';
 export { TagComponent } from './tag/tag.component';
